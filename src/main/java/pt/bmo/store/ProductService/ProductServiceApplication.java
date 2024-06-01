bac338449523ddfddbcb1eb0d29cd8dd34a49fec
Bruno Moreno <brunomorello7@gmail.com>
2024-06-01 16:59:34 +0100

    Added validations using jakarta Validations and Lookup event handler

--- src/main/java/pt/bmo/store/ProductService/ProductServiceApplication.java
+++ src/main/java/pt/bmo/store/ProductService/ProductServiceApplication.java
@@ -1,8 +1,12 @@
 package pt.bmo.store.ProductService;
 
+import org.axonframework.commandhandling.CommandBus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
+import pt.bmo.store.ProductService.commands.CreateProductCommandInterceptor;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,6 +14,11 @@ public class ProductServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+	@Autowired
+	public void registerInterceptor(ApplicationContext applicationContext, CommandBus commandBus) {
+		commandBus.registerDispatchInterceptor(applicationContext.getBean(CreateProductCommandInterceptor.class));
 	}
 
 }
--- src/main/java/pt/bmo/store/ProductService/commands/rest/CreateProductDto.java
+++ src/main/java/pt/bmo/store/ProductService/commands/rest/CreateProductDto.java
@@ -1,6 +1,19 @@
 package pt.bmo.store.ProductService.commands.rest;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+
 import java.math.BigDecimal;
 
-public record CreateProductDto(String title, BigDecimal price, Integer quantity) {
-}
+public record CreateProductDto(
+        @NotBlank(message = "Product title is mandatory")
+        String title,
+
+        @Positive(message = "Price cannot be negative or less than zero")
+        BigDecimal price,
+
+        @Min(value = 1, message = "Quantity must be at least 1")
+        @Max(value = 5, message = "Max quantity is 5")
+        Integer quantity) { }
